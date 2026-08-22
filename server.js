6bb0ace3b0d01e626d5ff30cfc019096193103fe
Yasmim <yasmim.mikelly08@gmail.com>
2026-08-22 09:17:31 -0300

    feat: adicionar tratamento de excecao

--- server.js
+++ server.js
@@ -1,4 +1,31 @@
 import http from 'node:http';
 
-http.createServer(() => {console.log('Knocked on the door') }).listen(3000); 
-// () => {} é uma Arrow function
+const users = [{
+  name: 'Alex Bessa',
+  email: 'alex@email.com'
+}, {
+  name: 'Valdiana Bessa',
+  email: 'valdiana@email.com'
+}, {
+  name: 'Ana Bessa',
+  email: 'ana@email.com'
+}];
+
+http.createServer((request, response) => {
+  if(request.url !== '/users'){
+    response.writeHead(
+        404,
+        {'content-type': 'application/json'}
+    )
+    response.end(JSON.stringify({
+        error: 404,
+        message: 'Resource not found.'
+    }));
+    return;
+  }
+
+  response.writeHead( 200, 
+    { "content-type": "application/json" } 
+); 
+    response.end(JSON.stringify(users));
+}).listen(3000);
