2684981457806ca146fe86921dcf8ed5a92573be
YASMIN MIKELLY DA SILVA PEREIRA <yasmimpereira6269@digitalcollege.internal>
2026-09-05 11:51:56 -0300

    implementar acesso de usuario cadastrado nos dados mocados

--- client/app.js
+++ client/app.js
@@ -1,23 +1,48 @@
 // Capturar elementos JS a partir do HTML
 
-const input = document.querySelector('input');
-const button = document.querySelector('button');
-const paragraph = document.querySelector('p');
+const btnIndex = document.querySelector('#btn-index');
+const email = document.querySelector('input[type="email"]')
+const password = document.querySelector('input[type="password"]')
+const btnLogin = document.querySelector('#btn-login')
+const btnHome = document.querySelector('#btn-home')
 
-button.addEventListener('click', async (event) => {
-    event.preventDefault()
+if (btnIndex) {
+  btnIndex.addEventListener('click', () => window.location.href = (
+ './pages/login.html'
+));
+}
 
-    const inputValue = input.value;
-    input.value = '';
+if (btnLogin) {
+// Faz a tela inicial ir para a de login
+  btnLogin.addEventListener('click', async (event) => {
+  event.preventDefault();
 
-    // o await torna o JS assíncrono (faz o JS esperar)
+  const emailValue = email.value;
+  const passwordValue = password.value;  
+
+  // Armazena uma resposta que irá chegar no sistema 
   const response = await fetch('http://localhost:3000/users');
+  // Transforma os dados em json
   const data = await response.json();
 
-  const user = data.find(user => user.email === inputValue);
+  const user = data.find((user) => (
+    user.email === emailValue && user.password === passwordValue
+  ));
 
-  if (!user) {
-    return paragraph.textContent = `Usuário não encontrado.`;
-  }
-  paragraph.textContent = `Usuário ${user.name} encontrado.`;
+  if(!user) return window.alert('Email e/ou senha inválidos.');
+
+  window.alert('Acesso permitido.')
+
+  window.location.href = './home.html';
+
 });
+}
+
+if (btnHome) {
+  btnHome.addEventListener('click', () => (
+  window.location.href = './login.html'
+));
+}
+
+
+
--- server/src/mocks/user.mock.js
+++ server/src/mocks/user.mock.js
@@ -1,12 +1,15 @@
 const users = [{
   name: 'Yasmim Mikelly',
-  email: 'yasmim@email.com'
+  email: 'yasmim@email.com',
+  password:'123'
 }, {
   name: 'Ravena',
-  email: 'ravena@email.com'
+  email: 'ravena@email.com',
+  password:'456'
 }, {
   name: 'Rihanna',
-  email: 'rihanna@email.com'
+  email: 'rihanna@email.com',
+  password:'789'
 }];
 
 export default users;
